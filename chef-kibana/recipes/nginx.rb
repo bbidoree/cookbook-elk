4fa3fa4f0f228cd1b275d0535fbdeca943674db4
EC2 Default User <ec2-user@croissant.localdomain>
2014-09-03 10:57:12 +0000

    add templates

--- chef-kibana/recipes/nginx.rb
+++
@@ -1,9 +0,0 @@
-template File.join(node['nginx']['dir'], 'sites-available', 'kibana') do
-  source 'nginx.erb'
-  owner node['nginx']['user']
-  mode '0644'
-  variables(
-    'root' => "#{node['kibana']['base_dir']}/current"
-  )
-end
-
